f196bd72146d4cefc1fd3567e578197a9bfb968d
Igor jakovljevic <igor.james.jakovljevic@gmail.com>
2014-09-02 23:42:18 +0200

    User Model Modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,43 @@
 class User < ActiveRecord::Base
+	attr_accessor :password
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :firstname, :presence => true
 	validates :lastname, :presence => true
-	validates :email, :presence => true, :format => {:with => email_regex }, :uniqueness => true
+	validates :email, :presence => true, :format => {:with => email_regex }
+	validates :password, :presence => true,:confirmation => true, :length=> { :within => 6..40 }
 
+	before_save :encrypt_password
+
+	def has_password?(submitted_password)
+	# Compare encrypted_password with the encrypted version of
+	# submitted_password.
+	return encrypted_password == encrypt(submitted_password)
+	end
+
+	def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+
+	private
+	 
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+
+	def encrypt(str)
+		return secure_hash("#{salt}--#{str}")
+	end
+
+	def secure_hash(str)
+		return Digest::SHA2.hexdigest(str)
+	end
+
+	def make_salt
+	secure_hash("#{Time.now.utc}--#{password}")
+	end
 end
+
