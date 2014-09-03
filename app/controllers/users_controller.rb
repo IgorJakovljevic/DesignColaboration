579b05489b3e3a7461dce7518988ace3c5ab496f
Igor jakovljevic <igor.james.jakovljevic@gmail.com>
2014-09-03 11:15:48 +0200

    User Signup done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.find(params[:id])
-	@title = @user.name
+  	@user = User.new
+	@title = "Sign Up"
+  end
+
+  def create
+	@user = User.new(user_params)
+	if @user.save
+		flash[:success] = "You have signed up"
+		redirect_to root_path
+	else
+		@title = "Sign up"
+		render 'new'
+	end
+  end
+
+  def user_params
+      params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	
 	attr_accessor :password
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
